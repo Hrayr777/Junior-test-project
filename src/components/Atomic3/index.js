d72683e3164147412cd4ef198bf2e51670af7ea9
ArturHovhannisyan999 <artur.hovhannisyan993@gmail.com>
2023-05-01 01:04:37 +0400

    atomic third desc

--- src/components/Atomic3/index.js
+++ src/components/Atomic3/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import styles from './Atomic3.module.css';
+import { images } from "../../images";
 
 
 const Atomic3 = () => {
     return (
         <div className={styles.atomicContainer}>
-
+            <div className={styles.atomicDesc}>
+                <div className={styles.atomicDescFirstLine}>
+                    <p className={styles.atomicDescFirstLineText}>THE DARK</p>
+                </div>
+                <div className={styles.atomicDescSecondLine}>
+                    <p className={styles.atomicDescSecondLineText}>LIGHT</p>
+                </div>
+            </div>
+            <p className={styles.atomicSubTitle}>
+                There are many variations of passages of Lorem Ipsum  majority have suffered alteration in some form.
+            </p>
+            <div className={styles.atomicDescLine}>
+                <div className={styles.atomicDescLineLeft}></div>
+                <p className={styles.atomicDescLineSpan}>*</p>
+                <div className={styles.atomicDescLineRight}></div>
+            </div>
+            <div className={styles.atomicDescFon}>
+                    <img alt={'lamp'} src={images.lamp}/>
+                <div className={styles.atomicDescParentCircle}>
+                    <div className={styles.atomicDescChildCircle}>
+                        <p className={styles.atomicDescCircleText1}>Majority</p>
+                        <p className={styles.atomicDescCircleText2}>The majority have suffered alteration in some form.</p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.atomicDescTitleDiv}>
+                <h2 className={styles.atomicDescTitle}>-John Abraham-</h2>
+                <p className={styles.atomicDescSubTitle}>The majority have suffered alteration in some form.</p>
+            </div>
         </div>
     );
 };
--- src/images/index.js
+++ src/images/index.js
@@ -1,9 +1,11 @@
 import trees from './trees.png';
 import atomicFon from './atomic-fon.png';
 import atomicTitle from './atomic-title.png';
+import lamp from './lamp.png';
 
 export const images = {
     trees,
     atomicFon,
-    atomicTitle
+    atomicTitle,
+    lamp
 }
